6b0978aacdc1e7206c63eda5b6c8e57639a8d10a
Karson Tran <karsont@uci.edu>
2023-07-24 15:40:21 -0700

    add switch statement for choice return

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 function getComputerChoice() {
     var choice = parseInt(Math.random() * 3);
+
+    switch(choice) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
 }
